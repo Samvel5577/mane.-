69eb9f9947521094268d7adedc4033443e81bbd2
Samvel <tradeup.education@gmail.com>
2024-07-31 02:39:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+number_of_tasks = 12
+print(number_of_tasks)
+number_of_hours = 1.5
+print(number_of_hours)
+course_name = 'Phyton'
+print(course_name)
+Time_for_the_task = 1.5/12
+print(1.5/12)
+
+print(course_name,number_of_tasks,number_of_hours,Time_for_the_task)
+
